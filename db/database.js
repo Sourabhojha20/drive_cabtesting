5132fd3dd4447c7a73f37c4f1a5d755f68c8f6d9
Sourabhojha20 <132444039+Sourabhojha20@users.noreply.github.com>
2025-05-03 18:36:23 +0530

    Update database.js

--- db/database.js
+++ db/database.js
@@ -1,7 +1,7 @@
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize('cabdrive', 'cabdrive', 'wqaDI%Y7}gpf', {
-  host: '44.227.217.144',
+const sequelize = new Sequelize('cabdrive', 'jsq6g7v75c3j6pm5xci5', 'pscale_pw_dG39MxErPcjKcAcpHF5zu4pIdh58FGhmyfi9WUMg455', {
+  host: 'aws.connect.psdb.cloud',
   dialect: 'mysql',
   dialectOptions: {
     ssl: {
@@ -10,15 +10,14 @@ const sequelize = new Sequelize('cabdrive', 'cabdrive', 'wqaDI%Y7}gpf', {
   }
 });
 
-
 const connectdb = async () => {
   try {
     await sequelize.authenticate();
-    console.log('✅ MySQL Connected');
+    console.log('✅ PlanetScale MySQL Connected');
   } catch (err) {
     console.error('❌ DB connection failed:', err);
   }
 };
 
 module.exports = connectdb;
-module.exports.sequelize = sequelize; 
+module.exports.sequelize = sequelize;
